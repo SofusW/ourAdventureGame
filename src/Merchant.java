3142f7bc26d28f05edd04917c170832e541b4120
sofuswinther <sfswnthr@gmail.com>
2025-04-07 14:44:00 +0200

    Lavet merchant, venter på item

--- src/Merchant.java
+++ src/Merchant.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Merchant {
+
+    List<item> items = new ArrayList<>();
+
+    public List<item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<item> items) {
+        this.items = items;
+    }
+
 }
