0cd518382c57155ceb6618c0d680a8b31182c60d
Zak24akh <zabe0001@stud.kea.dk>
2025-04-09 08:38:47 +0200

    Update Weapons.java

--- src/Weapons.java
+++ src/Weapons.java
@@ -1,2 +1,25 @@
+package game.items;
+
+public class Weapons extends Items {
+    private int damage;
+
+    public Weapons(String name, int damage) {
+        super(name); // Kalder superklassens constructor
+        this.damage = damage;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Weapon: " + getName() + ", Damage: " + damage);
+    }
+}
 public class Weapons {
 }
