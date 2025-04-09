8967a4a72186ccdc18f5eaa17fa9f6125f928018
Raged213 <raal0004@stud.kea.dk>
2025-04-09 09:50:34 +0200

    k

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+
 public interface GameCharacter {
+}
+
+
+class userCharacter implements GameCharacter {
+
+    public int Health(){
+        return 0;
+    }
+
+    public int attackPower(){
+        return 0;
+    }
+
+    public void name(){
+
+    }
+
+    public void Attack(){
+
+    }
+
+
+    public boolean alive(){
+        return false;
+    }
+
+
+    ArrayList<Item> items;
+
+    
+
 }
