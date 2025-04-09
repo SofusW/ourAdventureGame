d3dbbbb27bae999dbc6e29da57474f64c19aaa8f
Zak24akh <zabe0001@stud.kea.dk>
2025-04-09 08:37:53 +0200

    Update Relic.java

--- src/Relic.java
+++ src/Relic.java
@@ -1,2 +1,38 @@
-public class Relic {
+package game.items;
+
+public class Relics extends Items implements Comparable<Relics> {
+    private int value;
+    private boolean cursed;
+
+    public Relics(String name, int value, boolean cursed) {
+        super(name); // Kalder superklassens constructor
+        this.value = value;
+        this.cursed = cursed;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean isCursed() {
+        return cursed;
+    }
+
+    public void setCursed(boolean cursed) {
+        this.cursed = cursed;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Relic: " + getName() + ", Value: " + value + ", Cursed: " + cursed);
+    }
+
+    @Override
+    public int compareTo(Relics other) { // Sorterer efter værdi (dyreste først)
+        return Integer.compare(other.getValue(), this.getValue());
+    }
 }
